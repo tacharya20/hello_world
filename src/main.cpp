65d90c9a680da841eba1c4819b5c8d7813701404
Tejas H Acharya <89543503+tacharya20@users.noreply.github.com>
2021-08-27 18:59:48 -0700

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//Program Name: hello_world.cpp
+//Author: Tejas Harishchandra Acharya
+//Date: 27/08/2021
+//A simple program to print "Hello World" to the console.
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello World."<<endl; //prints "Hello World"
+    return 0;
+}
